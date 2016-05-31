98f1522fffb5485f7bed88a4da385634d759e3d0
Nadjmou BOINA <nadjmou.boina@gmail.com>
2016-05-31 22:15:18 +0200

    draft.1.0

--- mchezo/models.py
+++ mchezo/models.py
@@ -5,4 +5,20 @@ from django.db import models
 Class Person(models.Model):
     userid=models.CharField(max_lenght=30)
     mail=models.CharField(max_lenght=30)
-zdzadzad
+
+Class Questions(models.Model):
+    question_text=models.CharField(max_lenght=90)
+    pub_date=models.CharField(max_lenght=30)
+	#sent_by=foreign_key_userid?
+	
+Class QuestionChoice(models.Model):
+    userid=models.CharField(max_lenght=30)
+    mail=model.CharField(max_lenght=30)
+	
+Class Devinnette(models.Model):
+    choice_text=models.CharField(max_lenght=90)
+    pub_date=models.CharField(max_lenght=30)
+
+Class DevinnetteChoice(models.Model):
+    choice_text=models.CharField(max_lenght=90)
+    pub_date=models.CharField(max_lenght=30)
