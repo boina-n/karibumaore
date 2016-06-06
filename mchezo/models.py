daa0551c4a7fa0347b958a92caa0bed9ce5d1ff0
Nadjmou BOINA <nadjmou.boina@gmail.com>
2016-06-07 00:14:07 +0200

    20160607-NBO

--- mchezo/models.py
+++ mchezo/models.py
@@ -1,24 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    userid=models.CharField(max_length=200)
+    mail=models.EmailField(max_length=254)
 
-Class Person(models.Model):
-    userid=models.CharField(max_lenght=30)
-    mail=models.CharField(max_lenght=30)
+class Questions(models.Model):
+    question_text=models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.question_text
 
-Class Questions(models.Model):
-    question_text=models.CharField(max_lenght=90)
-    pub_date=models.CharField(max_lenght=30)
-	#sent_by=foreign_key_userid?
-	
-Class QuestionChoice(models.Model):
-    userid=models.CharField(max_lenght=30)
-    mail=model.CharField(max_lenght=30)
-	
-Class Devinnette(models.Model):
-    choice_text=models.CharField(max_lenght=90)
-    pub_date=models.CharField(max_lenght=30)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-Class DevinnetteChoice(models.Model):
-    choice_text=models.CharField(max_lenght=90)
-    pub_date=models.CharField(max_lenght=30)
+class QuestionChoice(models.Model):
+    choice_text=models.CharField(max_length=200)
+    pub_date=models.DateField()
+
+class Devinnette(models.Model):
+    choice_text=models.CharField(max_length=200)
+    pub_date=models.DateField()
+    #added_by=
+
+class DevinnetteChoice(models.Model):
+    choice_text=models.CharField(max_length=200)
+    pub_date=models.DateField()
--- mchezo/views.py
+++ mchezo/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Questions
 
 # Create your views here.
+
+def Questionslist(request):
+    latest_question_list = Questions.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('mchedzo/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
